dc4702c4f09c994a972b29c948d70eeb5abcdd0d
Philipp Brock <philipp.brock@idealo.de>
2019-05-13 08:17:29 +0200

    initial commit with spring-boot-app

---
+++ src/main/java/com/philippbro/fuelify/FuelifyApplication.java
@@ -0,0 +1,13 @@
+package com.philippbro.fuelify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelifyApplication.class, args);
+	}
+
+}
